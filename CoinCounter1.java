2c187b7d434775044d68165921cedde7d818f088
Johan Fautrier <johanfautrier@gmail.com>
2018-11-28 14:24:32 +0100

    Added idle animation facing correct way, added HUD images

---
+++ CoinCounter1.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class CoinCounter1 extends CoinHUD
+{
+    
+    
+    
+    public void act() 
+    {
+        
+    }   
+    
+}
--- level1.java
+++ level1.java
@@ -65,10 +65,10 @@ public class level1 extends World {
         KeyHUD keyHUD = new KeyHUD();
         LifeHUD lifeHUD = new LifeHUD();
 
-        addObject(coinHUD, 100, 40);        // Deze moet rechts boven scherm
-        addObject(crystalHUD, 200, 40);     // Deze onder Key
-        addObject(keyHUD, 300, 40);         // Deze onder levens
-        addObject(lifeHUD, 400, 40);        // Deze moet links boven scherm
+        addObject(coinHUD, 100, 220);        // Deze moet rechts boven scherm
+        addObject(crystalHUD, 100, 160);     // Deze onder Key
+        addObject(keyHUD, 100, 100);         // Deze onder levens
+        addObject(lifeHUD, 100, 40);        // Deze moet links boven scherm
 
         Music Music = new Music();
 
